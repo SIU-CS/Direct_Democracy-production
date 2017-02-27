f4bedcd6811794809b0cd9fae73c79a50dc064e8
Gregory Brinkman <gregorylucasbrinkman@gmail.com>
2017-02-27 17:53:46 -0600

    Revert "Merge remote-tracking branch 'refs/remotes/origin/dev'"
    This reverts commit 61b0747d4ec8e5d31399850ac9b23af90686bfc0.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from './Components/Main';
+import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <Main />,
+  <App />,
   document.getElementById('root')
 );
