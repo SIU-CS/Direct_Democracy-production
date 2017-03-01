48af70291e2b555d08c420e3daf4d2a2e4e71ead
Gregory Brinkman <gregorylucasbrinkman@gmail.com>
2017-02-28 19:59:33 -0600

    Vote Graph, resets on casted vote

--- src/Components/Bill.js
+++ src/Components/Bill.js
@@ -2,24 +2,39 @@ import React from 'react';
 import VoteOptions from './VoteOptions';
 import BillText from './BillText';
 import BillTitle from './BillTitle';
-/* import VoteGraph from './VoteGraph';*/
+import VoteGraph from './VoteGraph';
 
 class Bill extends React.Component {
 
 
     constructor(props){
         super(props);
-        this.state = {selected: 'none'}
-
+        this.state = {
+            selected: 'none',
+            for     : 1,
+            against : 1
+        }
     }
 
     onChildChanged(selectedVote){
+        var aye = this.state.for + 1;
+        var nay = this.state.against + 1;
+        if(selectedVote.localeCompare('aye') === 0){
+            this.setState({for: aye});
+        }else{
+            this.setState({against: nay});
+        }
         this.setState({selected: selectedVote}, function(){
-          //Send Post to DB
+            console.log();
+            //Send Post to DB
+
         });
     }
-  render() {
 
+
+
+
+  render() {
     var testTitle = "I'm Just a Bill";
     var testText  = "I'm just a bill "
 + "Yes I'm only a bill, "
@@ -32,9 +47,10 @@ class Bill extends React.Component {
 + "But today I am still just a bill.";
 
       // if userLoggedIn && userHasVoted
-	  if(this.state.selected.localeCompare('none') === 0){
+	    if((this.state.selected.localeCompare('none') === 0)){
       return (
         <div className="Bill">
+          <VoteGraph for={this.state.for} against={this.state.against}></VoteGraph>
           <BillTitle>
             {testTitle}
           </BillTitle>
@@ -56,6 +72,7 @@ class Bill extends React.Component {
             {testText}
           </BillText>
           <h3> The {this.state.selected}s have it!</h3>
+          <VoteGraph for={this.state.for} against={this.state.against}></VoteGraph>
         </div>
       );
     }
