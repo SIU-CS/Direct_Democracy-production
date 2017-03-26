8e925a8ce6c2a2bdfcdbefced82a64f6501dd0f8
Ewings-Goal <demarco.ewings@siu.edu>
2017-03-26 13:20:10 -0500

    Scrape data is now working

--- test/scrape.js
+++ test/scrape.js
@@ -5,9 +5,9 @@ request('https://www.whitehouse.gov/briefing-room/pending-legislation', function
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
     var parsedResults = [];
-    $(' a').each(function(i, element){
+    $('div.views-field-field-signed-date').each(function(i, element){
       // Select the previous element
-      var a = $(this).prev();
+      var a = $(this).next();
       // Get the rank by parsing the element two levels above the "a" element
       var rank = a.parent().parent().text();
       // Parse the link title
@@ -17,22 +17,22 @@ request('https://www.whitehouse.gov/briefing-room/pending-legislation', function
       // Get the subtext children from the next row in the HTML table.
       var subtext = a.parent().parent().next().children('.subtext').children();
       // Extract the relevant data from the children
-     var points = $(subtext).eq(0).text();
-     var username = $(subtext).eq(1).text();
-     var comments = $(subtext).eq(2).text();
-     // Our parsed meta data object
-     var metadata = {
-       rank: parseInt(rank),
-       title: title,
-       url: url,
-       points: parseInt(points),
-       username: username,
-       comments: parseInt(comments)
-     };
-     // Push meta-data into parsedResults array
-     parsedResults.push(metadata);
-   });
-   // Log our finished parse results in the terminal
-   console.log(parsedResults);
- }
+      var points = $(subtext).eq(0).text();
+      var username = $(subtext).eq(1).text();
+      var comments = $(subtext).eq(2).text();
+      // Our parsed meta data object
+      var metadata = {
+        rank: parseInt(rank),
+        title: title,
+    //    url: url,
+    //    points: parseInt(points),
+    //    username: username,
+    //    comments: parseInt(comments)
+      };
+      // Push meta-data into parsedResults array
+      parsedResults.push(metadata);
+    });
+    // Log our finished parse results in the terminal
+    console.log(parsedResults);
+  }
 });
