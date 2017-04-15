1f4c2e631b8248d14dba1effe00cc33e1fdf3f06
Tourk <chase.melvin@siu.edu>
2017-04-15 17:16:51 -0500

    Add files via upload

--- src/DDnative/src/BillPage.js
+++ src/DDnative/src/BillPage.js
@@ -5,83 +5,83 @@
  */
 
 import React, { Component } from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import {TouchableHighlight, StyleSheet, Text, View, Button, Navigator} from 'react-native';
+import ResponsiveImage from 'react-native-responsive-image';
 
 const onButtonPress = () => {
 	
 };
 
+
 class BillPage extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-	    <View style={styles.burgerContainer}>
-			<Button
-				title="Burger!"
-				onPress={onButtonPress}
-				color='black'/>
-		</View>
-		
-		<Text style={styles.billName}>
-		    (Bill title)
-		</Text>
-		
-		<Text style={styles.billByline}>
-			(Byline)
-		</Text>
-		
-		<Text style={styles.billText}>
-			(Bill Text)
-		</Text>
-		
-		<View style={styles.updownContainer}>
-			<Button
-				title="Up!"
-				onPress={onButtonPress}
-				color='green'/>
-			<Button
-				title="Down!"
-				onPress={onButtonPress}
-				color='red'/>
-		</View>
-      </View>
-    );
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.arrContainer}>
+					<TouchableHighlight underlayColor = 'rgba(255,255,255,.2)' onPress = {onButtonPress}>
+						<ResponsiveImage
+							source={require('./barrow.png')}
+							initWidth="60"
+							initHeight="50"/>
+					</TouchableHighlight>
+				</View>
+
+				<Text style={styles.billName}>
+					(Bill title)
+				</Text>
+				
+				<Text style={styles.billText}>
+					(Bill Text)
+				</Text>
+				
+				<View style={styles.updownContainer}>
+					<TouchableHighlight underlayColor = 'green' onPress={onButtonPress}>
+						<ResponsiveImage
+								source={require('./tup.png')}
+								initWidth="100"
+								initHeight="100"/>
+					</TouchableHighlight>
+					
+					<TouchableHighlight underlayColor = 'red' onPress={onButtonPress}>
+						<ResponsiveImage
+								source={require('./tdown.png')}
+								initWidth="100"
+								initHeight="100"/>
+					</TouchableHighlight>
+				</View>
+			</View>
+		);
   }
 }
 
 const styles = StyleSheet.create({
-  burgerContainer: {
-	  width: 50,
-  },
-  updownContainer: {
-	  justifyContent: 'space-around',
-	  flexDirection: 'row',
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#3498db',
-      padding: 20,
-  },
-  
-  billName: {
-	  fontSize: 35,
-	  fontWeight: 'bold',
-	  textAlign: 'center',
-	  marginTop: 30,
-	  marginBottom: 20,
+	arrContainer: {
+		flexDirection: 'row',
+		padding: 10,
+	},
+	updownContainer: {
+		justifyContent: 'space-around',
+		flexDirection: 'row',
+	},
+	container: {
+		flex: 1,
+		backgroundColor: '#3498db',
+	},
+	billName: {
+		fontSize: 35,
+		fontWeight: 'bold',
+		textAlign: 'center',
+		marginTop: 30,
+		marginBottom: 20,
 		color: '#fff',
-  },
-  billByline: {
-	  fontSize: 30,
-	  marginTop: 30,
-	  marginBottom: 20,
-	  color: '#fff',
-  },
-  billText: {
-	  fontSize: 18,
-	  marginTop: 30,
-	  marginBottom: 20,
-	  color: '#fff',
-  },
+	},
+	billText: {
+		fontSize: 18,
+		marginTop: 30,
+		marginBottom: 20,
+		color: '#fff',
+		padding: 20,
+	},
 });
+
 export default BillPage;
