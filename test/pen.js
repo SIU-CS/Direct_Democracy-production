b2a37df8a6878d6973bc8cfa1238eca2db381f6d
Ewings-Goal <demarco.ewings@siu.edu>
2017-04-16 15:19:34 -0500

    complete soon with scraping

---
+++ test/pen.js
@@ -0,0 +1,45 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
+request('https://www.whitehouse.gov/briefing-room/pending-legislation#', function (error, response, html) {
+  if (!error && response.statusCode == 200) {
+    var $ = cheerio.load(html);
+    var parsedResults = [];
+   $('div.views-field.views-field-field-signed-date').each(function(i, element){
+
+      // Select the previous element
+      var a = $(this).next();
+      // Get the rank by parsing the element two levels above the "a" element
+      var rank = a.parent().parent().text();
+      // Parse the link title
+  var title = a.text();
+// var url = $ .html();
+  var url = $('h3').html();
+var url = a.html();
+//  var url = a.html();
+
+
+  //    $('.legislation').attr('a', 'legislation').html()
+  //  var $ = ('.legislation').html()
+      // Get the subtext children from the next row in the HTML table.
+      var subtext = a.parent().parent().next().children('.subtext').children();
+      // Extract the relevant' data from the children
+      var points = $(subtext).eq(0).text();
+      var username = $(subtext).eq(1).text();
+      var comments = $(subtext).eq(2).text();
+      // Our parsed meta data object
+      var metadata = {
+        rank: parseInt(rank),
+        title: title,
+        url: url,
+        points: parseInt(points),
+        username: username,
+        comments: parseInt(comments)
+      };
+      // Push meta-data into parsedResults array
+      parsedResults.push(metadata);
+    });
+    // Log our finished parse results in the terminal
+    console.log(parsedResults);
+  }
+});
