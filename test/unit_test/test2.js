397edf5e3333d202843190aaf769c140942c07b4
Ewings-Goal <demarco.ewings@siu.edu>
2017-05-04 16:35:29 -0500

    unit testing

---
+++ test/unit_test/test2.js
@@ -0,0 +1,14 @@
+var expect = require('chai').expect;
+var assert = chai.assert;
+
+describe('Checking for the correct description for the ballt', function() {
+  it('The correct description should be displayed ', function() {
+      expect(true).to.be.true;
+  });
+
+});
+it("Correct usage of the description", function() {
+  assert.ok(true, 'The decription is correct if the element is coresponded with pre element');
+  assert.equal(pre.id.billTextContainer, 3);
+  
+});
